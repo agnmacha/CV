2b0f27b7cda1228ef720b4cb6a06aded8db8d3eb
Agnieszka Machałowska <agnieszka.machalowska@gmail.com>
2018-01-11 19:41:02 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-      <link href="http://fonts.googleapis.com/css?family=Aladin&subset=latin,latin-ext" rel='stylesheet' type='text/css'>
-      <link href="http://fonts.googleapis.com/css?family=Comfortaa&subset=latin,latin-ext" rel='stylesheet' type='text/css'>   
+      <link href="https://fonts.googleapis.com/css?family=Aladin&subset=latin,latin-ext" rel='stylesheet' type='text/css'>
+      <link href="https://fonts.googleapis.com/css?family=Comfortaa&subset=latin,latin-ext" rel='stylesheet' type='text/css'>   
    </head>
    <body>
       <nav class="navbar navbar-default navbar-fixed-top">
@@ -145,8 +145,10 @@
                      <div class="col-md-6 col-sm-12 col-xs-12form-group">
                         <input class="form-control"  name="email" placeholder="E-mail" type="email" required>
                      </div>
+                       <div class="col-md-12 col-sm-12 col-xs-12 form-group">
                      <textarea class="form-control"  name="message" placeholder="Wiadomość" rows="5" required></textarea>
                      <br>
+					  </div>
                      <div class="row">
                         <div class="col-md-12 form-group">
                            <button class="btn pull-right" type="submit" name="submit" >Wyślij</button>
